9d071516860e2380a43b123215d47963ee871bef
Tanya <htjuung@gmail.com>
2024-10-18 10:06:08 +0800

    deploy

---
+++ js/about.2d921a95.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkshoppingcart"]=self["webpackChunkshoppingcart"]||[]).push([[594],{603:function(n,t,u){u.r(t),u.d(t,{default:function(){return o}});var a=u(6768);const r={class:"about"};function s(n,t){return(0,a.uX)(),(0,a.CE)("div",r,t[0]||(t[0]=[(0,a.Lk)("h1",null,"This is an about page",-1)]))}var c=u(1241);const e={},i=(0,c.A)(e,[["render",s]]);var o=i}}]);
+//# sourceMappingURL=about.2d921a95.js.map
